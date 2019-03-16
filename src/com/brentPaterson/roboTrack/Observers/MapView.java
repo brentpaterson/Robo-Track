0cf48c6d0ee3f1c0834b0b2f3edecabb393245c5
Brent Paterson <bpaterson861@gmail.com>
2019-03-15 22:02:36 -0700

    fixed observers, no longer observable

--- src/com/brentPaterson/roboTrack/Observers/MapView.java
+++ src/com/brentPaterson/roboTrack/Observers/MapView.java
@@ -3,19 +3,7 @@ package com.brentPaterson.roboTrack.Observers;
 import java.util.Observable;
 import java.util.Observer;
 
-public class MapView implements IObservable, Observer {
-
-	@Override
-	public void addObserver(Observer obs) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void notifyObservers() {
-		// TODO Auto-generated method stub
-		
-	}
+public class MapView implements Observer {
 
 	@Override
 	public void update(Observable observable, Object data) {
