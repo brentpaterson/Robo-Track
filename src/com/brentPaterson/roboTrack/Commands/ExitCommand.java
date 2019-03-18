819ec0a1bd6f2347c483607a8bb058395d9ab546
Brent Paterson <bpaterson861@gmail.com>
2019-03-17 18:34:21 -0700

    exit command fully implemented

--- src/com/brentPaterson/roboTrack/Commands/ExitCommand.java
+++ src/com/brentPaterson/roboTrack/Commands/ExitCommand.java
@@ -2,6 +2,7 @@ package com.brentPaterson.roboTrack.Commands;
 
 import com.brentPaterson.roboTrack.GameWorld;
 import com.codename1.ui.Command;
+import com.codename1.ui.Dialog;
 import com.codename1.ui.events.ActionEvent;
 
 public class ExitCommand extends Command {
@@ -16,5 +17,17 @@ public class ExitCommand extends Command {
 	public void actionPerformed(ActionEvent ev) {
 		System.out.println("Exit game command invoked, displaying dialog...");
 		
+		Command cConfirm = new Command("Exit game");
+		Command cDeny = new Command("Cancel");
+		Command[] cmds = new Command[] {cConfirm, cDeny};
+		
+		Command c = Dialog.show("Are you sure you want to exit?",  "", cmds);
+		
+		if (c == cConfirm) {
+			System.out.println("Exit game confirmed, ending game...");
+			gwc.exit();
+		} else {
+			System.out.println("Exit game canceled");
+		}
 	}
 }
